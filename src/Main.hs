fed35942a04fc6e9822218f129d0bd1a74f74a92
mizon <mizon9@gmail.com>
2012-08-14 04:37:21 +0900

    some refactoring

--- src/Main.hs
+++ src/Main.hs
@@ -18,15 +18,10 @@ import Data.Enumerator
 import Snap.Util.FileServe
 import qualified Snap.Http.Server.Config as C
 import Snap
-import qualified Data.Text.Lazy.Encoding as TLE
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Encoding as TE
 import qualified Data.Text as T
 import qualified Data.ByteString.Char8 as BS
-import Data.Maybe
-import Data.Monoid
-
-import Development.Placeholders
 
 data Lupo = Lupo
     { _heist :: Snaplet (H.Heist Lupo)
@@ -61,9 +56,7 @@ instance EDB.MonadEntryDB (Handler Lupo Lupo) where
     getEntryDB = gets entryDB
 
 top :: Handler Lupo Lupo ()
-top = do
-    db <- EDB.getEntryDB
-    entries <- run_ =<< ($$) <$> EDB.all db <*> pure EL.consume
+top =
     H.renderWithSplices "index"
         [ ("page-title", textSplice "Lupo Web Diary")
         , ("style-sheet", textSplice "diary")
@@ -90,17 +83,12 @@ newEntry = method GET (newEntryEditor Nothing) <|> submitEntry
                 db <- EDB.getEntryDB
                 EDB.insert db $ EDB.Entry title body
                 redirect "/admin"
-            "Preview" -> do
-                spath <- submitPath
-                showPreview "New Entry: Preview" spath $ EDB.Entry title body
+            "Preview" ->
+                showPreview "New Entry: Preview" $ EDB.Entry title body
             "Edit" -> newEntryEditor $ Just $ EDB.Entry title body
             _ -> error "invalid request"
 
-    newEntryEditor :: Maybe EDB.Entry -> Handler Lupo Lupo ()
-    newEntryEditor entry = submitPath >>= \s -> showEditor "New Entry" s entry
-
-    submitPath :: MonadSnap m => m BS.ByteString
-    submitPath = getsRequest rqURI
+    newEntryEditor entry = showEditor "New Entry" entry
 
 editEntry :: Handler Lupo Lupo ()
 editEntry = method GET editor <|> method POST updateEntry
@@ -121,12 +109,11 @@ editEntry = method GET editor <|> method POST updateEntry
                 db <- EDB.getEntryDB
                 EDB.update db id_ $ EDB.Entry title body
                 redirect "/admin"
-            "Preview" -> submitPath >>= \s -> showPreview "Edit Entry: Preview" s $ EDB.Entry title body
+            "Preview" -> showPreview "Edit Entry: Preview" $ EDB.Entry title body
             "Edit" -> editEntryEditor $ EDB.Entry title body
             _ -> undefined
 
-    editEntryEditor entry = submitPath >>= \s -> showEditor "Edit Entry" s $ Just entry
-    submitPath = getsRequest rqURI
+    editEntryEditor entry = showEditor "Edit Entry" $ Just entry
 
 deleteEntry :: Handler Lupo Lupo ()
 deleteEntry = do
@@ -142,24 +129,28 @@ paramId = either undefined id . toIntegral <$> param "id"
 param :: MonadSnap m => BS.ByteString -> m T.Text
 param name = maybe undefined TE.decodeUtf8 <$> getParam name
 
-showPreview :: T.Text -> BS.ByteString -> EDB.Entry -> Handler Lupo Lupo ()
-showPreview prevTitle (TE.decodeUtf8 -> submitPath) e@EDB.Entry {..} = H.renderWithSplices "preview-entry"
-    [ ("style-sheet", textSplice "admin")
-    , ("preview-title", textSplice prevTitle)
-    , ("submit-path", textSplice submitPath)
-    , ("entry-title", textSplice $ TL.toStrict title)
-    , ("entry-body", textSplice $ TL.toStrict body)
-    , ("rendered-body", H.liftHeist $ V.entryBody e)
-    ]
+showPreview :: T.Text -> EDB.Entry -> Handler Lupo Lupo ()
+showPreview prevTitle e@EDB.Entry {..} = do
+    (TE.decodeUtf8 -> submitPath) <- getsRequest rqURI
+    H.renderWithSplices "preview-entry"
+        [ ("style-sheet", textSplice "admin")
+        , ("preview-title", textSplice prevTitle)
+        , ("submit-path", textSplice submitPath)
+        , ("entry-title", textSplice $ TL.toStrict title)
+        , ("entry-body", textSplice $ TL.toStrict body)
+        , ("rendered-body", H.liftHeist $ V.entryBody e)
+        ]
 
-showEditor :: T.Text -> BS.ByteString -> Maybe EDB.Entry -> Handler Lupo Lupo ()
-showEditor title (TE.decodeUtf8 -> submitPath) entry = H.renderWithSplices "edit-entry"
-    [ ("style-sheet", textSplice "admin")
-    , ("edit-title", textSplice title)
-    , ("submit-path", textSplice submitPath)
-    , ("default-title", textSplice $ maybe "" (TL.toStrict . EDB.title) entry)
-    , ("default-body", textSplice $ maybe "" (TL.toStrict . EDB.body) entry)
-    ]
+showEditor :: T.Text -> Maybe EDB.Entry -> Handler Lupo Lupo ()
+showEditor title entry = do
+    (TE.decodeUtf8 -> submitPath) <- getsRequest rqURI
+    H.renderWithSplices "edit-entry"
+        [ ("style-sheet", textSplice "admin")
+        , ("edit-title", textSplice title)
+        , ("submit-path", textSplice submitPath)
+        , ("default-title", textSplice $ maybe "" (TL.toStrict . EDB.title) entry)
+        , ("default-body", textSplice $ maybe "" (TL.toStrict . EDB.body) entry)
+        ]
 
 textSplice :: T.Text -> H.SnapletSplice Lupo Lupo
 textSplice = H.liftHeist . TH.textSplice
