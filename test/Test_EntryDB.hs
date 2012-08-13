2daee55616befd2d1151cc6a48ef870d6d897b05
mizon <mizon9@gmail.com>
2012-08-13 20:09:53 +0900

    extract module

--- test/Test_EntryDB.hs
+++ test/Test_EntryDB.hs
@@ -1,32 +1,93 @@
-{-# LANGUAGE FlexibleInstances
-    , FlexibleContexts
-    , UndecidableInstances
-    , GeneralizedNewtypeDeriving
-    , OverloadedStrings #-}
-module Test_EntryDB where
+{-# LANGUAGE OverloadedStrings
+    , ScopedTypeVariables
+    , FlexibleInstances #-}
+module Test_EntryDB
+    ( dbTest
+    ) where
 
 import qualified Lupo.EntryDB as EDB
+import Lupo.Exception
 import qualified Database.HDBC.Sqlite3 as Sqlite3
-import qualified Database.HDBC as HDB
-import qualified Data.Text as T
-import Test.HUnit
+import qualified Database.HDBC as DB
+import Test.Framework.Providers.HUnit
+import Test.Framework
+import Test.HUnit hiding (Test)
+import qualified Data.Enumerator.List as EL
+import Data.Enumerator
+import Control.Monad.CatchIO
 import Control.Monad.Reader
 import Control.Applicative
-import Control.Monad
-import Control.Monad.Trans
-import Data.Functor
+import qualified Control.Exception as E
+import qualified System.Directory as D
+import Prelude hiding (catch)
 
-instance EDB.MonadEntryDB (ReaderT EDB.EntryDB IO) where
+instance (MonadCatchIO m, Applicative m, Functor m) =>
+        EDB.MonadEntryDB (ReaderT EDB.EntryDB m) where
     getEntryDB = ask
 
-_test :: Test
-_test = TestCase $ do
-    edb <- EDB.makeEntryDB <$> (HDB.ConnWrapper <$> Sqlite3.connectSqlite3 "./test.sqlite3") <*> pure ""
-    flip runReaderT edb $ do
-        e <- EDB.select edb 1
-        liftIO $ assertEntry e 1 "fooo"
+dbTest :: Test
+dbTest = testGroup "database control"
+    [ dbTestCase "insert and select" $ do
+        db <- EDB.getEntryDB
+        EDB.insert db $ EDB.Entry "title" "body"
+        e <- EDB.select db 1
+        assertEntry (EDB.Entry "title" "body") e
 
-assertEntry :: EDB.Saved EDB.Entry -> Integer -> T.Text -> Assertion
-assertEntry e i title
-    | EDB.title (EDB.refObject e) == title && EDB.idx e == i = return ()
-    | otherwise = assertFailure "invlaid entry"
+    , dbTestCase "delete" $ do
+        db <- EDB.getEntryDB
+        EDB.insert db $ EDB.Entry "title" "body"
+        EDB.delete db 1
+        assertRaise RecordNotFound $
+            void $ EDB.select db 1
+
+    , dbTestCase "update" $ do
+        db <- EDB.getEntryDB
+        EDB.insert db $ EDB.Entry "title" "body"
+        EDB.update db 1 $ EDB.Entry "foo" "foooo"
+        e <- EDB.select db 1
+        assertEntry (EDB.Entry "foo" "foooo") e
+
+    , dbTestCase "all empty" $ do
+        db <- EDB.getEntryDB
+        enum <- EDB.all db
+        es <- run_ $ enum $$ EL.consume
+        liftIO $ Prelude.length es @?= 0
+
+    , dbTestCase "all exist" $ do
+        db <- EDB.getEntryDB
+        EDB.insert db $ EDB.Entry "foo1" "body"
+        EDB.insert db $ EDB.Entry "foo2" "body"
+        enum <- EDB.all db
+        es <- run_ $ enum $$ EL.consume
+        liftIO $ Prelude.length es @?= 2
+        assertEntry (EDB.Entry "foo2" "body") $ es !! 0
+        assertEntry (EDB.Entry "foo1" "body") $ es !! 1
+    ]
+
+assertEntry :: MonadIO m => EDB.Entry -> EDB.Saved EDB.Entry -> m ()
+assertEntry expected actual
+    | expected == EDB.refObject actual = return ()
+    | otherwise = liftIO $ assertFailure "invlaid entry"
+
+dbTestCase :: String -> ReaderT EDB.EntryDB IO () -> Test
+dbTestCase msg m = testCase msg $
+    bracket initialize finalize $ \conn ->
+        runReaderT m $ EDB.makeEntryDB conn entriesPath
+  where
+    initialize = do
+        D.createDirectory entriesPath
+        Sqlite3.connectSqlite3 "./test.sqlite3"
+
+    finalize conn = do
+        D.removeDirectoryRecursive entriesPath
+        void $ DB.run conn "DELETE FROM entries" []
+        DB.commit conn
+        DB.disconnect conn
+
+    entriesPath = "./tmp/entries"
+
+assertRaise :: (MonadCatchIO m, E.Exception e) => e -> m () -> m ()
+assertRaise ex m = (m >> liftIO (assertFailure "exception doesn't be raised")) `catch`
+    \(raised :: LupoException) -> do
+        unless (show ex == show raised) $
+            liftIO $ assertFailure "an exception raised but it isn't expected"
