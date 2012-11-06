856687e1b968508f86d0fe14399468cb7428b9e0
mizon <mizon9@gmail.com>
2012-11-06 12:53:30 +0900

    separate slow and fast tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,15 +1,26 @@
+module Main
+  ( main
+  , fast
+  ) where
+
+import Data.Monoid
+import Prelude hiding (all)
 import Test.Framework
 
-import Lupo.Test.Database
+import qualified Lupo.Test.Database as D
 import Lupo.Test.Navigation
 import Lupo.Test.Syntax
 import Lupo.Test.Util
 
-main :: IO ()
-main = defaultMain
-  [ dbTest
-  , savedTest
+main, fast :: IO ()
+main = defaultMain $ slowTests <> fastTests
+fast = defaultMain fastTests
+
+fastTests, slowTests :: [Test]
+fastTests =
+  [ D.savedTest
   , navigationTest
   , syntaxTest
   , utilTest
   ]
+slowTests = [D.dbTest]
