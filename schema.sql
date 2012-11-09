fbbdef130def9486e9e7214f83aad7c91fa4aedf
mizon <mizon9@gmail.com>
2012-11-09 21:22:24 +0900

    new table

--- schema.sql
+++ schema.sql
@@ -1,10 +1,20 @@
 CREATE TABLE entries (
-    id INTEGER PRIMARY KEY,
-    created_at TEXT NOT NULL,
-    modified_at TEXT NOT NULL,
-    day TEXT NOT NULL,
-    title TEXT NOT NULL,
-    body TEXT NOT NULL
-);
+    id INTEGER PRIMARY KEY
+  , created_at TEXT NOT NULL
+  , modified_at TEXT NOT NULL
+  , day TEXT NOT NULL
+  , title TEXT NOT NULL
+  , body TEXT NOT NULL
+  );
+
+CREATE TABLE comments (
+    id INTEGER PRIMARY KEY
+  , created_at TEXT NOT NULL
+  , modified_at TEXT NOT NULL
+  , day TEXT NOT NULL
+  , name TEXT NOT NULL
+  , body TEXT NOT NULL
+  );
 
 CREATE INDEX entries_day_index ON entries (day);
+CREATE INDEX comments_day_index ON comments (day);
