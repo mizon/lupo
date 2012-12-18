2e42d790ba26a033da3cf250be95a8d83505fb10
mizon <mizon9@gmail.com>
2012-12-19 02:55:48 +0900

    Refactoring URLMapper; extract time conversionc

--- src/Lupo/URLMapper.hs
+++ src/Lupo/URLMapper.hs
@@ -56,20 +56,22 @@ makeURLMapper :: Path -> URLMapper
 makeURLMapper basePath = URLMapper {
     entryPath = \LDB.Saved {..} -> fullPath' $ "entries" </> show idx
   , entryEditPath = \LDB.Saved {..} -> fullPath' $ "admin" </> show idx </> "edit"
-  , singleDayPath = fullPath' . T.unpack . formatTime "%Y%m%d"
-  , multiDaysPath = \d n -> fullPath' $ T.unpack [st|#{formatTime "%Y%m%d" d}-#{show n}|]
+  , singleDayPath = fullPath' . dayPath
+  , multiDaysPath = \d n -> fullPath' $ T.unpack [st|#{dayPath d}-#{show n}|]
   , monthPath = fullPath' . T.unpack . formatTime "%Y%m"
   , topPagePath = fullPath' ""
   , adminPath = fullPath' "admin"
   , loginPath = fullPath' "login"
   , initAccountPath = fullPath' "init-account"
-  , commentPostPath = \d -> fullPath' $ (T.unpack $ formatTime "%Y%m%d" d) </> "comment#new-comment"
-  , newCommentPath = \d -> fullPath' $ (T.unpack $ formatTime "%Y%m%d" d) <> "#new-comment"
-  , commentsPath = \d -> fullPath' $ (T.unpack $ formatTime "%Y%m%d" d) <> "#comments"
+  , commentPostPath = \d -> fullPath' $ dayPath d </> "comment#new-comment"
+  , newCommentPath = \d -> fullPath' $ dayPath d <> "#new-comment"
+  , commentsPath = \d -> fullPath' $ dayPath d <> "#comments"
   , cssPath = \(BS.unpack -> css) -> fullPath' $ "css" </> css
   , fullPath = \(BS.unpack -> path) -> fullPath' path
   }
   where
+    dayPath = T.unpack . formatTime "%Y%m%d"
+
     fullPath' = BS.pack . (BS.unpack basePath </>)
 
     p </> c = p <> "/" <> c
