f83f6c088ba303ed8c44bbac63e202b0263e93dc
mizon <mizon9@gmail.com>
2013-02-27 00:28:02 +0900

    Add some methods be compatible with old database to EntryDatabase

--- src/Lupo/Entry.hs
+++ src/Lupo/Entry.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE ViewPatterns #-}
 
 module Lupo.Entry
   ( EntryDatabase (..)
+  , Page (..)
+  , Saved (..)
   , makeEntryDatabase
+  , Entry (..)
+  , Comment (..)
   ) where
 
 import Control.Applicative
@@ -14,23 +20,35 @@ import Control.Monad.CatchIO
 import Control.Monad.Trans
 import qualified Data.Enumerator as E
 import qualified Data.Enumerator.Internal as EI
+import qualified Data.Enumerator.List as EL
 import qualified Data.Text as T
 import qualified Data.Time as Time
 import qualified Database.HDBC as DB
 import Prelude hiding (all)
 
 import Lupo.Exception
+import qualified Lupo.FieldValidator as FV
+import Lupo.Util
+
+class (MonadCatchIO m, Applicative m, Functor m) => DatabaseContext m
+instance (MonadCatchIO m, Applicative m, Functor m) => DatabaseContext m
 
 data EntryDatabase m = EntryDatabase
-  { select :: Integer -> m (Saved Entry)
-  , all :: forall a. E.Enumerator (Saved Entry) m a
+  { selectOne :: Integer -> m (Saved Entry)
+  , selectAll :: forall a. E.Enumerator (Saved Entry) m a
+  , insert :: Entry -> m ()
+  , update :: Integer -> Entry -> m ()
+  , delete :: Integer -> m ()
+  , beforeSavedDays :: forall a. Time.Day -> E.Enumerator Time.Day m a
+  , afterSavedDays :: forall a. Time.Day -> E.Enumerator Time.Day m a
+  , insertComment :: Time.Day -> Comment -> m ()
   }
 
-data Day = Day
-  { day :: Time.Day
-  , dayEntries :: [Saved Entry]
+data Page = Page
+  { pageDay :: Time.Day
+  , pageEntries :: [Saved Entry]
   , numOfComments :: Int
-  , dayComments :: [Saved Comment]
+  , pageComments :: [Saved Comment]
   } deriving (Eq, Show)
 
 data Saved o = Saved
@@ -41,17 +59,13 @@ data Saved o = Saved
   } deriving Show
 
 instance Eq o => Eq (Saved o) where
-  self == other =
-       idx self == idx other
-    && savedContent self == savedContent other
-
-class (MonadCatchIO m, Applicative m, Functor m) => DatabaseContext m
-instance (MonadCatchIO m, Applicative m, Functor m) => DatabaseContext m
+  self == other = idx self == idx other
+               && savedContent self == savedContent other
 
 data Entry = Entry
   { entryTitle :: T.Text
   , entryBody :: T.Text
-  } deriving (Eq, Show)
+  } deriving (Eq , Show)
 
 data Comment = Comment
   { commentName :: T.Text
@@ -61,33 +75,92 @@ data Comment = Comment
 makeEntryDatabase :: (DB.IConnection conn, DatabaseContext m) => conn -> IO (EntryDatabase m)
 makeEntryDatabase conn = do
   selectStatement <- DB.prepare conn "SELECT * FROM entries WHERE id = ?"
-  getAllStatement <- DB.prepare conn "SELECT * FROM entries ORDER BY created_at DESC"
+  selectAllStatement <- DB.prepare conn "SELECT * FROM entries ORDER BY created_at DESC"
+  insertStatement <- DB.prepare conn "INSERT INTO entries (created_at, modified_at, day, title, body) VALUES (?, ?, ?, ?, ?)"
+  updateStatement <- DB.prepare conn "UPDATE entries SET modified_at = ?, title = ?, body = ? WHERE id = ?"
+  deleteStatement <- DB.prepare conn "DELETE FROM entries WHERE id = ?"
+  beforeSavedDaysStatement <- DB.prepare conn "SELECT day FROM entries WHERE day <= ? GROUP BY day ORDER BY day DESC"
+  afterSavedDaysStatement <- DB.prepare conn "SELECT day FROM entries WHERE day >= ? GROUP BY day ORDER BY day ASC"
+  insertCommentStatement <- DB.prepare conn "INSERT INTO comments (created_at, modified_at, day, name, body) VALUES (?, ?, ?, ?, ?)"
   pure EntryDatabase
-    { select = \(DB.toSql -> id') -> liftIO $ DB.withTransaction conn $ const $ do
-        void $ DB.execute selectStatement [id']
-        row <- DB.fetchRow selectStatement
-        DB.finish selectStatement
-        maybe (throw RecordNotFound) (pure . sqlToEntry) row
+    { selectOne = \(DB.toSql -> id') -> liftIO $
+        DB.withTransaction conn $ const $ do
+          void $ DB.execute selectStatement [id']
+          row <- DB.fetchRow selectStatement
+          DB.finish selectStatement
+          maybe (throw RecordNotFound) (pure . sqlToEntry) row
 
-    , all = getAllEntries conn getAllStatement
+    , selectAll = enumStatement conn selectAllStatement [] E.$= EL.map sqlToEntry
+
+    , insert = \Entry {..} -> liftIO $ do
+        now <- Time.getZonedTime
+        DB.withTransaction conn $ const $
+          void $ DB.execute insertStatement
+            [ DB.toSql now
+            , DB.toSql now
+            , DB.toSql $ zonedDay now
+            , DB.toSql entryTitle
+            , DB.toSql entryBody
+            ]
+
+    , update = \i Entry{..} -> liftIO $ do
+        now <- Time.getZonedTime
+        DB.withTransaction conn $ const $
+          void $ DB.execute updateStatement
+            [ DB.toSql now
+            , DB.toSql entryTitle
+            , DB.toSql entryBody
+            , DB.toSql i
+            ]
+
+    , delete = \i -> liftIO $
+        DB.withTransaction conn $ const $ do
+          status <- DB.execute deleteStatement [DB.toSql i]
+          when (status /= 1) $
+            throw RecordNotFound
+
+    , beforeSavedDays = \(DB.toSql -> d) ->
+        enumStatement conn beforeSavedDaysStatement [d] E.$= EL.map (DB.fromSql . Prelude.head)
+
+    , afterSavedDays = \(DB.toSql -> d) ->
+        enumStatement conn afterSavedDaysStatement [d] E.$= EL.map (DB.fromSql . Prelude.head)
+
+    , insertComment = \d c@Comment {..} -> do
+        FV.validate commentValidator c
+        liftIO $ do
+          now <- Time.getZonedTime
+          DB.withTransaction conn $ const $
+            void $ DB.execute insertCommentStatement
+              [ DB.toSql now
+              , DB.toSql now
+              , DB.toSql d
+              , DB.toSql commentName
+              , DB.toSql commentBody
+              ]
     }
 
-getAllEntries :: (DB.IConnection conn, MonadIO m) => conn -> DB.Statement -> E.Enumerator (Saved Entry) m a
-getAllEntries conn stmt step = do
-  void $ liftIO $ DB.execute stmt []
+enumStatement :: (DB.IConnection conn, MonadIO m) => conn -> DB.Statement -> [DB.SqlValue] -> E.Enumerator [DB.SqlValue] m a
+enumStatement conn stmt values step = do
+  void $ liftIO $ DB.execute stmt values
   loop step
   where
     loop (E.Continue f) = do
-      e <- liftIO $ (sqlToEntry <$>) <$> DB.fetchRow stmt
+      e <- liftIO $ DB.fetchRow stmt
       case e of
         Just e' -> loop E.==<< f (E.Chunks [e'])
         Nothing -> f E.EOF
-    loop (E.Yield b s) = finalize *> EI.yield b s
     loop s = finalize *> EI.returnI s
 
     finalize = liftIO $ do
       DB.finish stmt
       DB.commit conn
+
+commentValidator :: FV.FieldValidator Comment
+commentValidator = FV.makeFieldValidator $ \Comment {..} -> do
+  FV.checkIsEmtpy commentName "Name"
+  FV.checkIsTooLong commentName "Name"
+  FV.checkIsEmtpy commentBody "Content"
+  FV.checkIsTooLong commentBody "Content"
 
 sqlToEntry :: [DB.SqlValue] -> Saved Entry
 sqlToEntry [ DB.fromSql -> id'
