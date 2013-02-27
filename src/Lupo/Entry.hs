7bf9c062b40d0b389ce652fd3467506f6165ac4a
mizon <mizon9@gmail.com>
2013-02-28 03:21:53 +0900

    Guard prepared statements with mutexes for concurrent executing

--- src/Lupo/Entry.hs
+++ src/Lupo/Entry.hs
@@ -15,6 +15,7 @@ module Lupo.Entry
   ) where
 
 import Control.Applicative
+import Control.Concurrent
 import Control.Monad
 import Control.Monad.CatchIO
 import Control.Monad.Trans
@@ -74,50 +75,54 @@ data Comment = Comment
 
 makeEntryDatabase :: (DB.IConnection conn, DatabaseContext m) => conn -> IO (EntryDatabase m)
 makeEntryDatabase conn = do
-  selectStatement <- DB.prepare conn "SELECT * FROM entries WHERE id = ?"
-  selectAllStatement <- DB.prepare conn "SELECT * FROM entries ORDER BY created_at DESC"
-  insertStatement <- DB.prepare conn "INSERT INTO entries (created_at, modified_at, day, title, body) VALUES (?, ?, ?, ?, ?)"
-  updateStatement <- DB.prepare conn "UPDATE entries SET modified_at = ?, title = ?, body = ? WHERE id = ?"
-  deleteStatement <- DB.prepare conn "DELETE FROM entries WHERE id = ?"
-  beforeSavedDaysStatement <- DB.prepare conn "SELECT day FROM entries WHERE day <= ? GROUP BY day ORDER BY day DESC"
-  afterSavedDaysStatement <- DB.prepare conn "SELECT day FROM entries WHERE day >= ? GROUP BY day ORDER BY day ASC"
-  insertCommentStatement <- DB.prepare conn "INSERT INTO comments (created_at, modified_at, day, name, body) VALUES (?, ?, ?, ?, ?)"
+  selectStatement <- prepareMutexStatement "SELECT * FROM entries WHERE id = ?"
+  selectAllStatement <- prepareMutexStatement "SELECT * FROM entries ORDER BY created_at DESC"
+  insertStatement <- prepareMutexStatement "INSERT INTO entries (created_at, modified_at, day, title, body) VALUES (?, ?, ?, ?, ?)"
+  updateStatement <- prepareMutexStatement "UPDATE entries SET modified_at = ?, title = ?, body = ? WHERE id = ?"
+  deleteStatement <- prepareMutexStatement "DELETE FROM entries WHERE id = ?"
+  beforeSavedDaysStatement <- prepareMutexStatement "SELECT day FROM entries WHERE day <= ? GROUP BY day ORDER BY day DESC"
+  afterSavedDaysStatement <- prepareMutexStatement "SELECT day FROM entries WHERE day >= ? GROUP BY day ORDER BY day ASC"
+  insertCommentStatement <- prepareMutexStatement "INSERT INTO comments (created_at, modified_at, day, name, body) VALUES (?, ?, ?, ?, ?)"
   pure EntryDatabase
-    { selectOne = \(DB.toSql -> id') -> liftIO $
-        DB.withTransaction conn $ const $ do
-          void $ DB.execute selectStatement [id']
-          row <- DB.fetchRow selectStatement
-          DB.finish selectStatement
-          maybe (throw RecordNotFound) (pure . sqlToEntry) row
+    { selectOne = \(DB.toSql -> id') ->
+        withTransactionGeneric conn $
+          withMutexStatement selectStatement $ \stmt ->
+            liftIO $ do
+              void $ DB.execute stmt [id']
+              row <- DB.fetchRow stmt
+              maybe (throw RecordNotFound) (pure . sqlToEntry) row
 
     , selectAll = enumStatement conn selectAllStatement [] E.$= EL.map sqlToEntry
 
-    , insert = \Entry {..} -> liftIO $ do
-        now <- Time.getZonedTime
-        DB.withTransaction conn $ const $
-          void $ DB.execute insertStatement
-            [ DB.toSql now
-            , DB.toSql now
-            , DB.toSql $ zonedDay now
-            , DB.toSql entryTitle
-            , DB.toSql entryBody
-            ]
+    , insert = \Entry {..} ->
+        withTransactionGeneric conn $
+          withMutexStatement insertStatement $ \stmt -> liftIO $ do
+            now <- Time.getZonedTime
+            void $ DB.execute stmt
+              [ DB.toSql now
+              , DB.toSql now
+              , DB.toSql $ zonedDay now
+              , DB.toSql entryTitle
+              , DB.toSql entryBody
+              ]
 
-    , update = \i Entry{..} -> liftIO $ do
-        now <- Time.getZonedTime
-        DB.withTransaction conn $ const $
-          void $ DB.execute updateStatement
-            [ DB.toSql now
-            , DB.toSql entryTitle
-            , DB.toSql entryBody
-            , DB.toSql i
-            ]
+    , update = \i Entry {..} ->
+        withTransactionGeneric conn $
+          withMutexStatement updateStatement $ \stmt -> liftIO $ do
+            now <- Time.getZonedTime
+            void $ DB.execute stmt
+              [ DB.toSql now
+              , DB.toSql entryTitle
+              , DB.toSql entryBody
+              , DB.toSql i
+              ]
 
-    , delete = \i -> liftIO $
-        DB.withTransaction conn $ const $ do
-          status <- DB.execute deleteStatement [DB.toSql i]
-          when (status /= 1) $
-            throw RecordNotFound
+    , delete = \(DB.toSql -> i) -> liftIO $
+        withTransactionGeneric conn $
+          withMutexStatement deleteStatement $ \stmt -> do
+            status <- liftIO $ DB.execute stmt [i]
+            when (status /= 1) $
+              throw RecordNotFound
 
     , beforeSavedDays = \(DB.toSql -> d) ->
         enumStatement conn beforeSavedDaysStatement [d] E.$= EL.map (DB.fromSql . Prelude.head)
@@ -127,10 +132,10 @@ makeEntryDatabase conn = do
 
     , insertComment = \d c@Comment {..} -> do
         FV.validate commentValidator c
-        liftIO $ do
-          now <- Time.getZonedTime
-          DB.withTransaction conn $ const $
-            void $ DB.execute insertCommentStatement
+        withTransactionGeneric conn $
+          withMutexStatement insertCommentStatement $ \stmt -> liftIO $ do
+            now <- Time.getZonedTime
+            void $ DB.execute stmt
               [ DB.toSql now
               , DB.toSql now
               , DB.toSql d
@@ -138,22 +143,20 @@ makeEntryDatabase conn = do
               , DB.toSql commentBody
               ]
     }
+    where
+      prepareMutexStatement = newMVar <=< DB.prepare conn
 
-enumStatement :: (DB.IConnection conn, MonadIO m) => conn -> DB.Statement -> [DB.SqlValue] -> E.Enumerator [DB.SqlValue] m a
-enumStatement conn stmt values step = do
-  void $ liftIO $ DB.execute stmt values
-  loop step
+enumStatement :: (DB.IConnection conn, Functor m, MonadCatchIO m) => conn -> MVar DB.Statement -> [DB.SqlValue] -> E.Enumerator [DB.SqlValue] m a
+enumStatement conn mutex values step =
+  withTransactionGeneric conn $
+    withMutexStatement mutex $ \stmt -> do
+      void $ liftIO $ DB.execute stmt values
+      loop stmt step
   where
-    loop (E.Continue f) = do
+    loop stmt (E.Continue f) = do
       e <- liftIO $ DB.fetchRow stmt
-      case e of
-        Just e' -> loop E.==<< f (E.Chunks [e'])
-        Nothing -> f E.EOF
-    loop s = finalize *> EI.returnI s
-
-    finalize = liftIO $ do
-      DB.finish stmt
-      DB.commit conn
+      loop stmt E.==<< f (maybe E.EOF (E.Chunks . pure) e)
+    loop _ s = EI.returnI s
 
 commentValidator :: FV.FieldValidator Comment
 commentValidator = FV.makeFieldValidator $ \Comment {..} -> do
@@ -176,3 +179,16 @@ sqlToEntry [ DB.fromSql -> id'
   , savedContent = Entry t b
   }
 sqlToEntry _ = error "in sql->entry conversion"
+
+withTransactionGeneric :: (Applicative m, MonadCatchIO m, DB.IConnection conn) => conn -> m a -> m a
+withTransactionGeneric conn action = onException action (liftIO $ DB.rollback conn)
+                                  <* liftIO (DB.commit conn)
+
+withMutexStatement :: MonadCatchIO m => MVar DB.Statement -> (DB.Statement -> m a) -> m a
+withMutexStatement mutex = bracket takeStatement finishStatement
+  where
+    takeStatement = liftIO $ takeMVar mutex
+
+    finishStatement stmt = liftIO $ do
+      DB.finish stmt
+      putMVar mutex stmt
